27882a4cfa7a90e0305499e3cf3bff63375ffb69
Dandy Naufaldi <dandy.naufaldi@gmail.com>
2017-12-08 21:19:14 +0700

    isi dikit2

--- BRASAT/BRASAT/MultiFrame.cpp
+++ BRASAT/BRASAT/MultiFrame.cpp
@@ -1,12 +1,66 @@
 #include "MultiFrame.h"
+#include "MainPanel.h"
+#include "PausePanel.h"
+#include "GamePanel.h"
+#include "HighScorePanel.h"
+#include "EpilogPanel.h"
 
 
-
-MultiFrame::MultiFrame()
+MultiFrame::MultiFrame(const wxString & title) : wxFrame(NULL, wxID_ANY, title)
 {
+	this->InitComponents();
 }
 
+void MultiFrame::InitComponents()
+{
+	boxSizer = new wxBoxSizer(wxVERTICAL);
+
+	this->mainPanel = new MainPanel(this);
+	this->mainPanel->Show(false);
+	this->boxSizer->Add(mainPanel, 1, wxEXPAND, 0);
+
+	this->gamePanel = new GamePanel(this);
+	this->gamePanel->Show(false);
+	this->boxSizer->Add(gamePanel, 1, wxEXPAND, 0);
+	
+	this->pausePanel = new PausePanel(this);
+	this->pausePanel->Show(false);
+	this->boxSizer->Add(pausePanel, 1, wxEXPAND, 0);
+	
+	this->highScorePanel = new HighScorePanel(this);
+	this->highScorePanel->Show(false);
+	this->boxSizer->Add(highScorePanel, 1, wxEXPAND, 0);
+	
+	this->epilogPanel = new EpilogPanel(this);
+	this->epilogPanel->Show(false);
+	this->boxSizer->Add(epilogPanel, 1, wxEXPAND, 0);
+
+	SetSizer(boxSizer);
+
+	ShowMainPanel();
+}
+
+void MultiFrame::ShowMainPanel()
+{
+	this->mainPanel->Show(true);
+	this->gamePanel->Show(false);
+	this->pausePanel->Show(false);
+
+}
 
 MultiFrame::~MultiFrame()
 {
+	delete mainPanel;
+	delete gamePanel;
+	delete pausePanel;
+	delete highScorePanel;
+	delete epilogPanel;
+}
+
+void MultiFrame::fitWindowSize()
+{
+	this->boxSizer->Fit(this);
+	this->boxSizer->SetSizeHints(this);
+	this->SetSize(wxSize(1280, 720));
+	this->Layout();
 }
--- BRASAT/BRASAT/MultiFrame.h
+++ BRASAT/BRASAT/MultiFrame.h
@@ -1,8 +1,30 @@
 #pragma once
-class MultiFrame
+#include "wx\wx.h"
+
+class MainPanel;
+class GamePanel;
+class PausePanel;
+class HighScorePanel;
+class EpilogPanel;
+
+class MultiFrame : public wxFrame
 {
+private:
+	wxBoxSizer* boxSizer;
+	MainPanel* mainPanel;
+	GamePanel* gamePanel;
+	PausePanel* pausePanel;
+	HighScorePanel* highScorePanel;
+	EpilogPanel* epilogPanel;
+
+	void fitWindowSize();
 public:
-	MultiFrame();
+	MultiFrame(const wxString &title);
+	void InitComponents();
+	void ShowMainPanel();
+	void ShowPausePanel();
+	void ShowHighScorePanel();
+	void SHowEpilogPanel();
 	~MultiFrame();
 };
 
